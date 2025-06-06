a95166dc4ae289b247c1d533aeba73369191dbbe
Dolly136 <dolly.r.wz@gmail.com>
2025-06-06 17:07:25 +0530

    icon changes

--- utils/svg_icon_list.js
+++ utils/svg_icon_list.js
@@ -1,9 +1,16 @@
-export const SVG_LIST = Array.from({ length: 1021 }, (_, i) => {
-  const id = i + 1;
-  const padded = String(id).padStart(2, "0");
-  return {
-    id,
-    name: padded,
-    url: `/images/svg_icon/${padded}.svg`,
-  };
-});
+export const SVG_LIST = [
+  {
+    id: 0,
+    name: "Trimmo",
+    url: "https://trimmo.bio/images/Trimmo-Logo.svg",
+  },
+  ...Array.from({ length: 1021 }, (_, i) => {
+    const id = i + 1;
+    const padded = String(id).padStart(2, "0");
+    return {
+      id,
+      name: padded,
+      url: `/images/svg_icon/${padded}.svg`,
+    };
+  }),
+];
